8613c15993c14cdefd48928e6bb681a92a49ce9c
jdrumgoole <joe@joedrumgoole.com>
2019-04-01 08:46:24 +0100

    Made pytho 3 compliant

--- _6_manyarticles.py
+++ _6_manyarticles.py
@@ -3,22 +3,26 @@ import string
 import datetime
 import random
 
-def randomString(size, letters=string.letters):
-    return "".join([random.choice(letters) for _ in xrange(size)])
 
-def makeArticle(count ):
+def randomString(size, letters=string.ascii_letters):
+    return "".join([random.choice(letters) for _ in range(size)])
+
+
+def makeArticle(count):
     return {"_id": count,
-            "title": "Title " + str( count ),
+            "title": "Title " + str(count),
             "body": randomString(80),
-            "author": "USER_" + str( random.randrange( 0, 999999 )),
+            "author": "USER_" + str(random.randrange(0, 999999)),
             "postdate": datetime.datetime.now()}
 
-def makeUser( count ):
-    return {"_id": "USER_" + str( count ),
+
+def makeUser(count):
+    return {"_id": "USER_" + str(count),
             "password": randomString(10),
             "karma": random.randint(0, 500),
-            "registered" : datetime.datetime.utcnow(),
+            "registered": datetime.datetime.utcnow(),
             "lang": "EN"}
+
 
 client = pymongo.MongoClient()
 
@@ -31,25 +35,26 @@ articlesCollection.drop()
 
 users = []
 count = 0
-for i in range(100000):
 
-    users.append( makeUser( i ))
+size = 100000
+for i in range(size):
 
-    if ( len( users ) % 1000  ) == 0 :
-        usersCollection.insert_many( users )
+    users.append(makeUser(i))
+
+    if (len(users) % 1000) == 0:
+        usersCollection.insert_many(users)
         count = count + 1000
-        print( "inserted %i users" % count )
+        print("inserted %i users" % count)
         users = []
 
 articles = []
 count = 0
 
-for i in range(100000) :
-    articles.append( makeArticle(i ))
+for i in range(size):
+    articles.append(makeArticle(i))
 
-    if (len( articles ) % 1000 ) == 0 :
-        articlesCollection.insert_many( articles )
+    if (len(articles) % 1000) == 0:
+        articlesCollection.insert_many(articles)
         count = count + 1000
-        print( "inserted %i articles" % count )
+        print("inserted %i articles" % count)
         articles = []
-
